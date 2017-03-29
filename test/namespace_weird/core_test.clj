ee3387531483a3fc072f891efb28f8bee71508a0
ChipNowacek <chip@twostewards.com>
2017-03-29 18:39:43 -0400

    lein new app namespace-weird --force

---
+++ test/namespace_weird/core_test.clj
@@ -0,0 +1,7 @@
+(ns namespace-weird.core-test
+  (:require [clojure.test :refer :all]
+            [namespace-weird.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
